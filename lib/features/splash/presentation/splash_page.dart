a5dbbb615720580d55a1831617b7300f2ba8e749
faris <faris.k218@gmail.com>
2024-10-05 10:52:06 +0400

    feat(splash): added splash screen

---
+++ lib/features/splash/presentation/splash_page.dart
@@ -0,0 +1,92 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:flutter_trading_app/core/constants/assets.dart';
+import 'package:flutter_trading_app/features/home/presentation/home_page.dart';
+
+class SplashPage extends StatefulWidget {
+  const SplashPage({super.key});
+
+  @override
+  SplashPageState createState() => SplashPageState();
+}
+
+class SplashPageState extends State<SplashPage>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _scaleAnimation;
+  late Animation<double> _opacityAnimation;
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 400),
+    );
+    _scaleAnimation = Tween<double>(
+      begin: 0.8,
+      end: 1.0,
+    ).animate(
+      CurvedAnimation(
+        parent: _controller,
+        curve: Curves.easeInOut,
+      ),
+    );
+    _opacityAnimation = Tween<double>(
+      begin: 0.0,
+      end: 1.0,
+    ).animate(
+      CurvedAnimation(
+        parent: _controller,
+        curve: Curves.easeInOut,
+      ),
+    );
+    _controller.forward();
+
+    //To handle the hero animation:
+    Timer(const Duration(seconds: 2), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(
+          builder: (context) => const HomePage(),
+        ),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: AnimatedBuilder(
+          animation: _controller,
+          builder: (context, child) {
+            return Opacity(
+              opacity: _opacityAnimation.value,
+              child: Transform.scale(
+                scale: _scaleAnimation.value,
+                child: Hero(
+                  tag: "logo",
+                  child: SvgPicture.asset(
+                    BRAND_LOGO_SVG,
+                    semanticsLabel: 'Exinity',
+                    width: 250,
+                  ),
+                ),
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
--- lib/router/app_router.dart
+++ lib/router/app_router.dart
@@ -3,34 +3,32 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_trading_app/features/forex/presentation/bloc/forex_bloc.dart';
 import 'package:flutter_trading_app/features/forex/presentation/bloc/forex_event.dart';
 import 'package:flutter_trading_app/features/forex/presentation/pages/forex_list_page.dart';
+import 'package:flutter_trading_app/features/home/presentation/home_page.dart';
+import 'package:flutter_trading_app/features/splash/presentation/splash_page.dart';
 import 'package:flutter_trading_app/injection_container.dart';
 
 import 'irouter_provider.dart';
 
 class AppRouter extends IRouterProvider {
-  static const home = "/";
-  static const instrumentDetail = "/instrument/detail";
+  static const splash = "/";
+  static const home = "/home";
+  static const forex = "/forex";
 
   @override
   void initRouter(FluroRouter router) {
-    // Home page (Forex List Page)
+    router.define(splash, handler: Handler(handlerFunc: (context, params) {
+      return const SplashPage();
+    }));
+
     router.define(home, handler: Handler(handlerFunc: (context, params) {
+      return const HomePage();
+    }), transitionType: TransitionType.none);
+
+    router.define(forex, handler: Handler(handlerFunc: (context, params) {
       return BlocProvider(
         create: (context) => sl<ForexBloc>()..add(LoadForexInstruments()),
         child: const ForexListPage(),
       );
     }));
-
-    // Instrument detail page
-    // Commented out for now, as it's not implemented yet
-    // router.define(instrumentDetail, handler: Handler(handlerFunc: (context, params) {
-    //   final instrument = context!.settings!.arguments as ForexInstrument;
-    //   return BlocProvider(
-    //     create: (context) => sl<ForexBloc>(),
-    //     child: InstrumentDetailPage(instrument: instrument),
-    //   );
-    // }));
-
-    // Add more routes as needed
   }
 }
