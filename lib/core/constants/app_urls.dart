a2703d3bc84df31a031db6dc9d4270eaa4f8bccb
faris <faris.k218@gmail.com>
2024-10-05 13:21:33 +0400

    feature(env): moved api key into env

--- lib/core/constants/app_urls.dart
+++ lib/core/constants/app_urls.dart
@@ -1,6 +1,9 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
 class ApiConstants {
   static const String baseUrl = 'https://finnhub.io/api/v1';
-  static const String apiKey = 'cs0fdspr01qrbtrlbpd0cs0fdspr01qrbtrlbpdg';
+
+  static String get apiKey => dotenv.env['FINNHUB_API_KEY'] ?? '';
 
   static String get forexSymbols =>
       '$baseUrl/forex/symbol?exchange=oanda&token=$apiKey';
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 import 'app.dart';
 import 'injection_container.dart' as AppDependencies;
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: ".env");
   await AppDependencies.init();
   runApp(MyApp());
 }
